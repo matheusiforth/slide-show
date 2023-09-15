4da08a4076359a9432a30197e26d7553ddfdbde7
matheusiforth <matheus@iforth.com.br>
2023-09-15 16:55:19 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactergversfgverfgvgrthbrbhrs
+          Learn Reactergversfgverfgvgrthbrbhrsvfgwedgvwsedv
         </a>
       </header>
     </div>
