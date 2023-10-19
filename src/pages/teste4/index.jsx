dfabf41087a4ec9a35b43b070b6741752b242c59
matheusiforth <matheus@iforth.com.br>
2023-10-19 08:07:29 -0300

    comit

---
+++ src/pages/teste4/index.jsx
@@ -0,0 +1,10 @@
+import GraficoVertical from "../../componentes/graficoVertical";
+
+export default function Teste4() {
+    return (
+        <div style={{ background: 'black', width: '100vw', height: '70vh' }}>
+            {/* <h1>srlghsrohgtoirhgrshgiuhsru</h1> */}
+            <GraficoVertical gh={true}/>
+        </div>
+    )
+}
--- src/util/global/converteJson.jsx
+++ src/util/global/converteJson.jsx
@@ -43,7 +43,6 @@ export function ConverteJson(props) {
         }
     ]
 
-
     return {
         teste
     }
